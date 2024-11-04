7132a7d5364d400a9d1e8356b084d69a56d6f212
assef <fernandoassef@hotmail.com>
2024-11-03 23:49:25 -0600

    build: hamming (manual mode)

---
+++ src/Hamming/Program.cs
@@ -0,0 +1,96 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+
+namespace Hamming
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int m;
+            int n;
+            List<Pattern> patterns = new();
+            Matrix<double> weightMatrix;
+            Vector<double> bias;
+            double epsilon;
+            bool converged = false;
+
+            #region User inputs vectors
+            Console.Write("Cantidad de vectores a ingresar (M): ");
+            m = int.Parse(Console.ReadLine()!);
+
+            Console.Write("Cantidad de neuronas de cada vector (N): ");
+            n = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < m; i++)
+            {
+                Console.Write(string.Format("Vector X{0} (separado por espacios): ", i));
+                Vector<double> vector = Utils.GetVectorFromUser(n);
+
+                patterns.Add(new Pattern($"X{i}", vector));
+            }
+
+            List<Vector<double>> vectors = patterns.Select(x => x.Vector).ToList(); // extracts Vector property of patterns
+            #endregion
+
+            weightMatrix = Utils.BuildWeightMatrix(vectors, m, n);
+            bias = Utils.BuildBiasMatrix(m, n);
+            epsilon = Utils.GetEpsilon(n);
+
+            Console.Clear();
+
+            #region Print values
+            Console.WriteLine("Matriz de pesos");
+            Console.WriteLine(weightMatrix.ToMatrixString());
+            
+            Console.WriteLine("BIAS");
+            Console.WriteLine(bias.ToVectorString());
+            
+            Console.WriteLine("Epsilon");
+            Console.WriteLine(epsilon);
+
+            Console.WriteLine("\n=======================================================\n");
+            #endregion
+
+            while (true)
+            {
+                int i = 0;
+                
+                #region User inputs test vector
+                Console.Write("Vector de prueba (separado por espacios): ");
+                Vector<double> testVector = Utils.GetVectorFromUser(n);
+                #endregion
+
+                Vector<double> vector = Utils.GetFirstOutput(testVector, n, weightMatrix, bias).ApplyTransferFunction();
+                Console.WriteLine($"U({i}) = {Utils.FormatVector(vector)}");
+                
+                while (!converged)
+                {
+                    if (!vector.HasDuplicateElements())
+                    {
+                        i++;
+                    
+                        vector = Utils.HammingFormula(vector, epsilon).ApplyTransferFunction();
+                        Console.WriteLine($"U({i}) = {Utils.FormatVector(vector)}");
+
+                        int? positiveIndex = Utils.CheckForPositive(vector);
+                    
+                        if (positiveIndex != null)
+                        {
+                            Console.WriteLine($"La RN converge y asocia {Utils.FormatVector(testVector)} con {patterns[positiveIndex.Value].Name} = {Utils.FormatVector(patterns[positiveIndex.Value].Vector)}");
+                            converged = true;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"La RN no asociará con ningún patrón porque contiene al menos 2 elementos iguales.");
+                        converged = true;
+                    }
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("=======================================================");
+                converged = false;
+            }
+        }
+    }
+}
---
+++ src/Hamming/Utils.cs
@@ -0,0 +1,157 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+
+namespace Hamming
+{
+    public class Pattern
+    {
+        public string Name { get; set; }
+        public Vector<double> Vector { get; set; }
+
+        public Pattern(string name, Vector<double> vector)
+        {
+            Name = name;
+            Vector = vector;
+        }
+    }
+
+    public static class Utils
+    {
+        public static string FormatVector(Vector<double> vector)
+        {
+            string result = "[ ";
+            
+            for (int i = 0; i < vector.Count; i++)
+            {
+                result += vector[i] + " ";
+            }
+
+            result += "]";
+
+            return result;
+        }
+
+        public static Vector<double> GetVectorFromUser(int vectorSize)
+        {
+            List<double> row = Console.ReadLine()!.Split(' ').Select(double.Parse).ToList();
+            Vector<double> vector = Vector<double>.Build.Dense(vectorSize);
+
+            for (int j = 0; j < row.Count; j++)
+            {
+                vector[j] = row[j];
+            }
+
+            return vector;
+        }
+
+        public static Matrix<double> BuildWeightMatrix(List<Vector<double>> vectors, int m, int n)
+        {
+            Matrix<double> weightMatrix = Matrix<double>.Build.Dense(m, n); // initializes mxn matrix
+            
+            for (int row = 0; row < m; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    weightMatrix[row, col] = 0.5 * vectors[row][col];
+                }
+            }
+
+            return weightMatrix;
+        }
+
+        public static Vector<double> BuildBiasMatrix(int m, int n)
+        {
+            Vector<double> bias = Vector<double>.Build.Dense(m);
+
+            for (int i = 0; i < m; i++)
+            {
+                bias[i] = (double)n / 2;
+            }
+
+            return bias;
+        }
+
+        public static double GetEpsilon(int n)
+        {
+            return (double)1 / (n - 1);
+        }
+
+        public static double HammingTransferFunction(double value)
+        {
+            return value > 1 ? 1 : value < 0 ? 0 : value;
+        }
+
+        public static Vector<double> ApplyTransferFunction(this Vector<double> vector)
+        {
+            for (int i = 0; i < vector.Count; i++)
+            {
+                vector[i] = HammingTransferFunction(vector[i]);
+            }
+
+            return vector;
+        }
+
+        public static Vector<double> GetFirstOutput(Vector<double> testVector, int n, Matrix<double> weightMatrix, Vector<double> bias)
+        {
+            return (1 / (double)n) * ((weightMatrix * testVector) + bias);
+        }
+
+        public static Vector<double> HammingFormula(Vector<double> vector, double epsilon)
+        {
+            int length = vector.Count;
+            Vector<double> result = Vector<double>.Build.Dense(length);
+
+            for (int i = 0; i < length; i++)
+            {
+                double sum = 0;
+
+                for (int j = 0; j < length; j++)
+                {
+                    if (j != i)
+                    {
+                        sum += vector[j];
+                    }
+                }
+
+                result[i] = vector[i] - (epsilon * sum);
+            }
+
+            return result;
+        }
+
+        public static int? CheckForPositive(Vector<double> vector)
+        {
+            int? positiveIndex = null;
+            int positiveCount = 0;
+
+            for (int i = 0; i < vector.Count; i++)
+            {
+                if (vector[i] > 0)
+                {
+                    positiveCount++;
+                    if (positiveCount > 1)
+                    {
+                        return null;
+                    }
+                    positiveIndex = i;
+                }
+            }
+
+            return positiveCount == 1 ? positiveIndex : null;
+        }
+
+        public static bool HasDuplicateElements(this Vector<double> vector)
+        {
+            HashSet<double> uniqueElements = new();
+
+            foreach (double value in vector)
+            {
+                if (!uniqueElements.Add(value))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
